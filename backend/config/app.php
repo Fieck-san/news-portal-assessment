8a83a0377b63ee5ec13e905acd45d7e26dde4e3b
Syafiq <fieck@Syafiqs-MacBook-Pro.local>
2026-07-23 00:01:50 +0800

    initial setup for backend and frontend via docker

---
+++ backend/config/app.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\ServiceProvider;
+
+return [
+    'name' => env('APP_NAME', 'Kini News Assessment'),
+    'env' => env('APP_ENV', 'production'),
+    'debug' => (bool) env('APP_DEBUG', false),
+    'url' => env('APP_URL', 'http://localhost'),
+    'timezone' => 'Asia/Kuala_Lumpur',
+    'locale' => 'en',
+    'fallback_locale' => 'en',
+    'faker_locale' => 'en_US',
+    'key' => env('APP_KEY'),
+    'cipher' => 'AES-256-CBC',
+    'maintenance' => [
+        'driver' => 'file',
+    ],
+    'providers' => ServiceProvider::defaultProviders()->toArray(),
+];
+
---
+++ backend/routes/console.php
@@ -0,0 +1,6 @@
+<?php
+
+use Illuminate\Foundation\Console\AboutCommand;
+
+AboutCommand::add('Assessment', fn () => ['Project' => 'Laravel and Angular News Portal']);
+
